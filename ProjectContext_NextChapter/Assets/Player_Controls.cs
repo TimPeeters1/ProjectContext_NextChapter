6cb8440ab2b88c4d55e1168d075a3ed026c1a47f
Tim Peeters <tim.peeters12345@gmail.com>
2020-02-04 14:51:59 +0100

    Added Cinemachine + Player Controller Setup

---
+++ ProjectContext_NextChapter/Assets/Player_Controls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerController
+{
+    Player1,
+    Player2
+}
+
+public class Player_Controls : MonoBehaviour
+{
+
+    public PlayerController thisPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(thisPlayer == PlayerController.Player1)
+        {
+
+        }
+        if(thisPlayer == PlayerController.Player2)
+        {
+
+        }
+    }
+}
