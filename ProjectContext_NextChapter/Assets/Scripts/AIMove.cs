c542633053072c7c364e3ac7c0da5466294d6344
방저녁 <hyeok020514@naver.com>
2020-02-05 23:51:09 +0900

    Made Market Consumer Ai and prefab too.they move to point that you want and wait few seconds! what a nice

---
+++ ProjectContext_NextChapter/Assets/Scripts/AIMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMove : MonoBehaviour
+{
+    enum AIMovement
+    {
+        Stay,
+        Move,
+    }
+    private NavMeshAgent agent;
+    private Rigidbody AIrigid;
+    private AIMovement AIState;
+
+    int CurrentTargetNumber = 0;
+    private Vector3 CurrentTarget;
+
+    public  GameObject[] targetArray;
+
+    float PointDistance;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        AIrigid = GetComponent<Rigidbody>();
+
+        AIState = AIMovement.Move;
+        StartCoroutine("StayFewSeconds");
+    }
+
+    void Update()
+    {
+        switch (AIState)
+        {
+            case AIMovement.Move:
+        PointDistance = Vector3.Distance(transform.position, CurrentTarget);
+
+        if(PointDistance <= 3)
+        {
+                    AIState = AIMovement.Stay;
+        }
+        else
+        {
+            agent.SetDestination(CurrentTarget);
+        }
+                break;
+            case AIMovement.Stay:
+                StartCoroutine("StayFewSeconds");
+                break;
+        }
+    }
+
+    IEnumerator StayFewSeconds()
+    {
+        CurrentTargetNumber = Random.Range(1, targetArray.Length);
+        yield return new WaitForSeconds(Random.Range(2.0f, 6.0f));
+
+        CurrentTarget = targetArray[CurrentTargetNumber].transform.position;
+
+        AIState = AIMovement.Move;
+    }
+}
